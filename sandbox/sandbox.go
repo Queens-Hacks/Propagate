860a46c627c7eab43f778d7ca380de7ff374617a
Erik Pilkington <erik.pilkington@gmail.com>
2015-09-19 14:44:39 -0400

    fixed bug, i think

--- sandbox/sandbox.go
+++ sandbox/sandbox.go
@@ -19,7 +19,7 @@ const (
 
 type NewState struct {
 	Operation StateChange
-	Dir Direction
+	Dir       Direction
 }
 
 type Node struct {
@@ -69,20 +69,26 @@ const (
 	Undef
 )
 
-func watchLuaThread(l *lua.State, d time.Duration) {
-	end_time := time.Now().Add(d)
-	setLuaTimeoutHook(l, 500, func() {
-		if time.Now().After(end_time) {
+const duration time.Duration = 500 * time.Millisecond
+
+func updateEndTime(t *time.Time) {
+	*t = time.Now().Add(duration)
+}
+
+func watchLuaThread(l *lua.State, end_time *time.Time) {
+	setLuaTimeoutHook(l, func() {
+		if time.Now().After(*end_time) {
 			panic("AAAAHHHH!!!")
 		}
 	})
-	l.ProtectedCall(0, lua.MultipleReturns, 0)
+
+	l.ProtectedCall(0, lua.MaskCall, 0)
 }
 
-func setLuaTimeoutHook(l *lua.State, count int, callback func()) {
+func setLuaTimeoutHook(l *lua.State, callback func()) {
 	lua.SetDebugHook(l, func(l *lua.State, ar lua.Debug) {
 		callback()
-	}, lua.MaskCount, count)
+	}, lua.MaskCount, 500)
 }
 
 func addIntFunc(l *lua.State, name string, fn func(*lua.State, int) int) {
@@ -161,10 +167,13 @@ func runNode(node internalNode) {
 	}()
 
 	l := lua.NewState()
+	var end_time time.Time
+	updateEndTime(&end_time)
 
 	world := <-node.resume
 
 	addDirFunc(l, "grow", func(l *lua.State, d Direction) int {
+		updateEndTime(&end_time)
 		var state NewState
 		state.Dir = d
 		state.Operation = Move
@@ -177,6 +186,7 @@ func runNode(node internalNode) {
 	})
 
 	addVoidFunc(l, "wait", func(l *lua.State) int {
+		updateEndTime(&end_time)
 		var state NewState
 		state.Dir = Undef
 		state.Operation = Wait
@@ -188,6 +198,7 @@ func runNode(node internalNode) {
 	})
 
 	addDirFunc(l, "split", func(l *lua.State, d Direction) int {
+		updateEndTime(&end_time)
 		var state NewState
 		state.Dir = d
 		state.Operation = Split
@@ -205,5 +216,5 @@ func runNode(node internalNode) {
 	})
 
 	lua.LoadString(l, node.program)
-	watchLuaThread(l, time.Duration(500)*time.Millisecond)
+	watchLuaThread(l, &end_time)
 }

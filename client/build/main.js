54521de37a7e9d16f3d749e76ef3bab7ad91ccbd
Max Bittker <max@linux-feoj.site>
2015-09-19 09:39:22 -0400

    prepping to handle diffs

--- client/build/main.js
+++ client/build/main.js
@@ -4,44 +4,33 @@ var husl = require('husl')
 var canvas = document.getElementById('art');
 var ctx = canvas.getContext('2d');
 
-var tilesize = 1;
-var n = 0;
 
-var sx = canvas.width/50;
-var sy = canvas.height/50;
+var sx = canvas.width / 50;
+var sy = canvas.height / 50;
 
-var colorMap = [30,210];
-
-// (function animloop() {
-//     requestAnimationFrame(animloop);
-//     render();
-//     // n++
-// })();
+var colorMap = [30, 210];
+var firstFrame = true;
 
 function render(world) {
 
     for (var y = 0; y < world.length; y++) {
         for (var x = 0; x < world[y].length; x++) {
-            // console.log(world[y][x])
-            ctx.fillStyle = husl.toHex(colorMap[world[y][x]['tileType']] , 50, 50);
-            ctx.fillRect(x *sx , y*sy , sx, sy);
+            drawTile(x, y, world[y][x]['tileType']);
         }
     }
 
-    // for (var x = 0; x < canvas.width; x += tilesize) {
-    //     for (var y = 0; y < canvas.height; y += tilesize) {
-    //         // n = (n + 1) % 6;
-    //         // ctx.fillStyle = husl.toHex(50, 0, n * 11);
-    //         n += tilesize / 2
-    //         ctx.fillStyle = husl.toHex(n % 360, 50, 50);
+}
 
-    //         ctx.fillRect(x, y, tilesize, tilesize);
+function renderDelta(delta) {
+    for (var i = 0; i < delta.length; i++) {
+        drawTile(delta[i]['loc']['x'], delta[i]['loc']['x'], delta['i']['tileType']);
 
-    //     }
-    //     // n++;
-    // }
+    }
+}
 
-
+function drawTile(x, y, type) {
+    ctx.fillStyle = husl.toHex(colorMap[type], 50, 50);
+    ctx.fillRect(x * sx, y * sy, sx, sy);
 }
 
 
@@ -50,25 +39,47 @@ var ws = new WebSocket("ws://localhost:4444/");
 
 ws.onmessage = function(evt) {
 
-
     var reader = new FileReader();
     reader.addEventListener("loadend", function() {
         json = JSON.parse(reader.result)
             // console.log(json);
-        render(json['world']);
+
+        if (firstFrame) {
+
+            gameheight = json['world'].length;
+            gamewidth = json['world'][0].length;
+
+            // ctx.canvas.width = window.innerWidth;
+            // ctx.canvas.height = window.innerHeight;
+            
+            sx = canvas.width / gameheight;
+            sy = canvas.height / gamewidth;
+
+            render(json['world']);
+            firstFrame = 0;
+
+        } else
+            renderDelta(json['delta'])
+
     });
     reader.readAsText(evt.data);
 
 };
 
 ws.onopen = function() {
-    console.log("Connection established, handle with function");
+    console.log("Connection established");
 };
 
 
-ws.onclose = function() {
-    // websocket is closed.
-    console.log("Connection is closed...");
+// ws.onclose = function() {
+//     // websocket is closed.
+//     console.log("Connection is closed...");
+// };
+
+
+window.onbeforeunload = function() {
+    ws.onclose = function() {}; // disable onclose handler first
+    ws.close()
 };
 
 },{"husl":2}],2:[function(require,module,exports){

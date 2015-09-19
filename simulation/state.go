b11f71c0d9a3a5f61d1fffa3c09c9ea1fb762c67
Jake Pittis <jakepittis@gmail.com>
2015-09-19 00:51:31 -0400

    added basic world state

--- simulation/state.go
+++ simulation/state.go
@@ -1,1 +1,23 @@
 package simulation
+
+type plant struct {
+	color string
+}
+
+type tileType int
+
+const (
+	dirtTile tileType = iota
+	airTile
+	plantTile
+)
+
+type tile struct {
+	t       tileType
+	plantId int
+}
+
+type state struct {
+	world  [][]tile
+	plants map[int]plant
+}
